7a561a00baeb6f80ecc648f8df3d6b4741f43450
Vacumsio <39467630+Vacumsio@users.noreply.github.com>
2021-12-21 21:42:30 +0300

    ще настройки

--- Source/GeometrySandBox/Private/BaseGeometryActor.cpp
+++ Source/GeometrySandBox/Private/BaseGeometryActor.cpp
@@ -6,7 +6,7 @@
 // Sets default values
 ABaseGeometryActor::ABaseGeometryActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
@@ -15,7 +15,12 @@ ABaseGeometryActor::ABaseGeometryActor()
 void ABaseGeometryActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	//UE_LOG(LogTemp, Display, TEXT("Hello Unreal!"));
+	//UE_LOG(LogTemp, Warning, TEXT("Hello Unreal!"));
+	//UE_LOG(LogTemp, Error, TEXT("Hello Unreal!"));
+
+	PrintTypes();
 }
 
 // Called every frame
@@ -23,5 +28,20 @@ void ABaseGeometryActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void ABaseGeometryActor::PrintTypes()
+{
+	int WeaponsNum = 4;
+	int KillsNum = 7;
+	float Health = 34.1679834;
+	bool IsDead = false;
+	bool HasWeapon = true;
+
+	UE_LOG(LogTemp, Display, TEXT("WEapons num: %d, kill num %i"), WeaponsNum, KillsNum);
+	UE_LOG(LogTemp, Display, TEXT("Health %.f"), Health);
+	UE_LOG(LogTemp, Display, TEXT("Health %.2f"), Health);
+	UE_LOG(LogTemp, Display, TEXT("Is dead %d"), IsDead);
+	UE_LOG(LogTemp, Display, TEXT("Has Weapon %d"), static_cast<int>(HasWeapon));
 }
 
